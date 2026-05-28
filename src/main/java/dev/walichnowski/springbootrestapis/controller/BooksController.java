bf43246ea041bfa8e73ad49089059b4a71b38f97
Kacper Walichnowski <kacperw112@wp.pl>
2026-05-28 17:39:29 +0200

    Add book entity and books controller

---
+++ src/main/java/dev/walichnowski/springbootrestapis/controller/BooksController.java
@@ -0,0 +1,42 @@
+package dev.walichnowski.springbootrestapis.controller;
+
+import dev.walichnowski.springbootrestapis.entity.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BooksController
+{
+    private final List<Book> books = new ArrayList<>();
+
+    public BooksController()
+    {
+        initialiseBooks();
+    }
+
+    private void initialiseBooks()
+    {
+        books.addAll(List.of(
+                new Book("Title 1", "Author 1", "fantasy"),
+                new Book("Title 2", "Author 2", "science"),
+                new Book("Title 3", "Author 3", "it"),
+                new Book("Title 4", "Author 4", "fantasy"),
+                new Book("Title 5", "Author 5", "it")
+        ));
+    }
+
+    @GetMapping("/api")
+    public String helloAPI()
+    {
+        return "Hello from API!";
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks()
+    {
+        return books;
+    }
+}
