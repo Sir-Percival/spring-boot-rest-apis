ee08925dbd49f33edd55653060f3ee69adf771a1
Kacper Walichnowski <kacperw112@wp.pl>
2026-05-28 22:20:52 +0200

    Add POST, PUT and DELETE endpoints

--- src/main/java/dev/walichnowski/springbootrestapis/controller/BooksController.java
+++ src/main/java/dev/walichnowski/springbootrestapis/controller/BooksController.java
@@ -1,10 +1,10 @@
 package dev.walichnowski.springbootrestapis.controller;
 
 import dev.walichnowski.springbootrestapis.entity.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,5 +54,36 @@ public class BooksController
                 .filter(book -> book.getTitle().equalsIgnoreCase(title))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @PostMapping("/api/books")
+    public void createBook(@RequestBody Book newBook)
+    {
+        boolean isNewBook = books.stream()
+                .noneMatch(book -> book.getTitle().equalsIgnoreCase(newBook.getTitle()));
+
+        if(isNewBook)
+            books.add(newBook);
+    }
+
+    @PutMapping("/api/books/{title}")
+    public void updateBook(@PathVariable String title, @RequestBody Book updatedBook)
+    {
+        for(int i = 0; i < books.size(); i++)
+        {
+            if(books.get(i).getTitle().equalsIgnoreCase(title))
+            {
+                books.set(i, updatedBook);
+                return;
+            }
+        }
+    }
+
+    @Operation(summary = "Delete book by title", description = "Deletes book by provided title")
+    @ApiResponse(responseCode = "200", description = "Successfully deleted")
+    @DeleteMapping("/api/books/{title}")
+    public void deleteBook(@PathVariable @Parameter(name = "title", description = "Book's title", example = "Title 1") String title)
+    {
+        books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
     }
 }
