21dfcb01a62b13a4edba3867a2723df799878de7
Kacper Walichnowski <kacperw112@wp.pl>
2026-05-28 17:04:44 +0200

    Initial commit

---
+++ src/main/java/dev/walichnowski/springbootrestapis/SpringBootRestApisApplication.java
@@ -0,0 +1,15 @@
+package dev.walichnowski.springbootrestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApisApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootRestApisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/walichnowski/springbootrestapis/SpringBootRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.walichnowski.springbootrestapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
