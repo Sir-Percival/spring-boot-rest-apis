764682a025c2c0342eb9cd70ac4d58ddb2eeeaf9
Kacper Walichnowski <kacperw112@wp.pl>
2026-05-30 22:15:16 +0200

    Add Swagger and data validation

--- src/main/java/dev/walichnowski/springbootrestapis/controller/EmployeeController.java
+++ src/main/java/dev/walichnowski/springbootrestapis/controller/EmployeeController.java
@@ -1,15 +1,19 @@
 package dev.walichnowski.springbootrestapis.controller;
 
 import dev.walichnowski.springbootrestapis.entity.Employee;
-import dev.walichnowski.springbootrestapis.repository.EmployeeRepository;
+import dev.walichnowski.springbootrestapis.request.EmployeeRequest;
 import dev.walichnowski.springbootrestapis.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Employees controller", description = "Operations related to employees")
 @RestController
 @RequestMapping("/api/employees")
 @AllArgsConstructor
@@ -18,8 +22,52 @@ public class EmployeeController
     private final EmployeeService service;
 
     @GetMapping
+    @Operation(summary = "Get all employees", description = "Retrieve a list of all employees")
     public List<Employee> findAll()
     {
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Fetch single employee by Id")
+    public Employee getEmployeeById(@PathVariable @Min(1) long id)
+    {
+        return service.findById(id);
+    }
+
+    @GetMapping("/age")
+    @Operation(summary = "Get all employees ordered by age desc")
+    public List<Employee> getAllByAgeDesc()
+    {
+        return service.findAllOrderByAgeDesc();
+    }
+
+    @GetMapping("/age/{age}")
+    @Operation(summary = "Get all employees older than given age")
+    public List<Employee> getAllOlderThan(@Min(18) @PathVariable int age)
+    {
+        return service.findAllOlderThan(age);
+    }
+
+    @Operation(summary = "Create a new employee")
+    @PostMapping
+    public Employee addEmployee(@Valid @RequestBody EmployeeRequest request)
+    {
+        return service.save(request);
+    }
+
+    @Operation(summary = "Update an employee")
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable @Min(1) long id, @Valid @RequestBody EmployeeRequest request)
+    {
+        return service.update(id, request);
+    }
+
+    @Operation(summary = "Delete an employee")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable @Min(1) long id)
+    {
+        service.deleteById(id);
     }
 }
--- src/main/java/dev/walichnowski/springbootrestapis/request/EmployeeRequest.java
+++ src/main/java/dev/walichnowski/springbootrestapis/request/EmployeeRequest.java
@@ -1,5 +1,6 @@
 package dev.walichnowski.springbootrestapis.request;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,8 +8,20 @@ import lombok.Data;
 @AllArgsConstructor
 public class EmployeeRequest
 {
+    @NotBlank(message = "First name is mandatory")
+    @Size(min = 2, max = 30, message = "First name must be between 2 and 50 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min = 2, max = 30, message = "Last name must be between 2 and 50 characters")
     private String lastName;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email
     private String email;
+
+    @NotNull
+    @Min(18)
+    @Max(90)
     private int age;
 }
