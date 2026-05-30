58d4df4d430903ef5855db9331fcdd21d85c7ef3
Kacper Walichnowski <kacperw112@wp.pl>
2026-05-30 13:30:12 +0200

    Add H2 DB configuration and Employee entity

---
+++ src/main/java/dev/walichnowski/springbootrestapis/entity/Employee.java
@@ -0,0 +1,27 @@
+package dev.walichnowski.springbootrestapis.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+    private int age;
+}
