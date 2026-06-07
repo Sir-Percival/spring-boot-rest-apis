355cfd050ed49de731b9ee1286383642dfc2d160
Kacper Walichnowski <kacperw112@wp.pl>
2026-06-07 20:07:30 +0200

    Add security request matchers

--- src/main/java/dev/walichnowski/springbootrestapis/security/SecurityConfig.java
+++ src/main/java/dev/walichnowski/springbootrestapis/security/SecurityConfig.java
@@ -1,10 +1,15 @@
 package dev.walichnowski.springbootrestapis.security;
 
+import org.springframework.boot.http.converter.autoconfigure.ClientHttpMessageConvertersCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig
@@ -31,5 +36,26 @@ public class SecurityConfig
                 .build();
 
         return new InMemoryUserDetailsManager(john, jane, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, ClientHttpMessageConvertersCustomizer clientConvertersCustomizer) throws Exception
+    {
+        http.authorizeHttpRequests(customizer ->
+                customizer
+                        .requestMatchers("/docs/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/h2/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // Use HTTP Basic Authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
